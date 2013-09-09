baf42f06ee729e31405e6f2f98b634f235f85ad0
YellowAfterlife <yellowafterlife@ymail.com>
2013-09-09 23:06:47 +0300

    More of rectangle functions
    Now even with Flash11-specific ones.

--- flash/geom/Rectangle.hx
+++ flash/geom/Rectangle.hx
@@ -12,9 +12,9 @@ class Rectangle {
 	public var top(get, set):Float;
 	public var bottom(get, set):Float;
 	// Point fields:
-	//public var size(get, set):Point;
-	//public var topLeft(get, set):Point;
-	//public var bottomRight(get, set):Point;
+	public var size(get, set):Point;
+	public var topLeft(get, set):Point;
+	public var bottomRight(get, set):Point;
 	// Meta:
 	public function new(a:Float = 0, b:Float = 0, c:Float = 0, d:Float = 0):Void {
 		this.x = a;
@@ -28,8 +28,20 @@ class Rectangle {
 	public function equals(o:Rectangle):Bool {
 		return x == o.x && y == o.y && width == o.width && height == o.height;
 	}
+	public function isEmpty():Bool {
+		return width <= 0 || height <= 0;
+	}
 	public function setEmpty():Void {
 		x = y = width = height = 0;
+	}
+	public function copyFrom(o:Rectangle):Void {
+		x = o.x;
+		y = o.y;
+		width = o.width;
+		height = o.height;
+	}
+	public function setTo(a:Float, b:Float, c:Float, d:Float):Void {
+		x = a; y = b; width = c; height = d;
 	}
 	public function setVoid():Void {
 		//x = y = 0x7fffffff;
@@ -48,6 +60,13 @@ class Rectangle {
 	private inline function set_right(v:Float):Float { width = v - x; return v; }
 	private inline function get_bottom():Float { return y + height; }
 	private inline function set_bottom(v:Float):Float { height = v - y; return v; }
+	// Point fields:
+	private inline function get_size():Point { return new Point(width, height); }
+	private inline function set_size(v:Point):Point { width = v.x; height = v.y; return v.clone(); }
+	private inline function get_topLeft():Point { return new Point(x, y); }
+	private inline function set_topLeft(v:Point):Point { width = v.x; height = v.y; return v.clone(); }
+	private inline function get_bottomRight():Point { return new Point(x + width, y + height); }
+	private inline function set_bottomRight(v:Point):Point { width = v.x - x; height = v.y - y; return v.clone(); }
 	// Contains:
 	public function contains(u:Float, v:Float):Bool {
 		return (u -= x) >= 0
@@ -64,13 +83,21 @@ class Rectangle {
 		: o.x >= x && o.y >= y && o.right <= right && o.bottom <= bottom;
 	}
 	//
+	public function intersection(o:Rectangle):Rectangle {
+		var x0:Float, x1:Float, y0:Float, y1:Float, a:Float, b:Float;
+		return
+		((x0 = (a = x) < (b = o.x) ? b : a)
+		<= (x1 = (a += width) > (b += o.width) ? b : a))
+		&& ((y0 = (a = y) < (b = o.y) ? b : a)
+		<= (y1 = (a += height) > (b += o.height) ? b : a))
+		? new Rectangle(x0, y0, x1 - x0, y1 - y0) : new Rectangle();
+	}
 	public function intersects(o:Rectangle):Bool {
 		var x0, x1, y0, y1;
 		return (x0 = (x < (x0 = o.x) ? x0 : x))
 			<= (x1 = (right > (x1 = o.right) ? x1 : right))
 			? false : (y0 = (y < (y0 = o.y) ? y0 : y))
 			<= (y1 = (bottom > (y1 = o.bottom) ? y1 : y));
-		
 	}
 	//
 	public function join(o:Rectangle):Void {
@@ -80,7 +107,27 @@ class Rectangle {
 		if ((v = o.right - right) > 0) { width += v; }
 		if ((v = o.bottom - bottom) > 0) { height += v; }
 	}
+	public function union(o:Rectangle):Rectangle {
+		var a:Float, b:Float, c:Float, d:Float;
+		return new Rectangle(
+			(a = x) < (c = o.x) ? a : c,
+			(b = y) < (d = o.y) ? b : d,
+			(a += width) > (c += o.width) ? a : c,
+			(b += height) > (d += o.height) ? b : d);
+	}
 	//
+	public function inflate(u:Float, v:Float):Void {
+		x -= u; y -= v; width += u * 2; height += v * 2;
+	}
+	public function inflatePoint(v:Point):Void {
+		inflate(v.x, v.y);
+	}
+	public function offset(u:Float, v:Float):Void {
+		x += u; y += v;
+	}
+	public function offsetPoint(o:Point):Void {
+		x += o.x; y += o.y;
+	}
 	public function transform(m:Matrix):Void {
 		var v, l, t, r, b;
 		// top left (default):
