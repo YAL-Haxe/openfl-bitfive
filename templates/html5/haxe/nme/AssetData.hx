4634c9f3686177b94bebb1f676b38803d372bd52
YellowAfterlife <yellowafterlife@ymail.com>
2013-12-26 19:47:19 +0200

    Fixed BitmapData caching returning same nodes.
    And there's now also a bitfive-specific Assets class. Yay for overrides!

--- templates/html5/haxe/nme/AssetData.hx
+++ templates/html5/haxe/nme/AssetData.hx
@@ -5,35 +5,26 @@ import openfl.Assets;
 
 
 class AssetData {
-
-	
-	public static var className = new Map <String, Dynamic> ();
-	public static var library = new Map <String, LibraryType> ();
-	public static var path = new Map <String, String> ();
-	public static var type = new Map <String, AssetType> ();
-	
+	//
+	public static var className:Map<String, Dynamic> = new Map<String, Dynamic>();
+	public static var library:Map<String, LibraryType> = new Map<String, LibraryType>();
+	public static var path:Map<String, String> = new Map<String, String>();
+	public static var type:Map<String, AssetType> = new Map<String, AssetType>();
+	//
 	private static var initialized:Bool = false;
 	
-	
 	public static function initialize ():Void {
-		
 		if (!initialized) {
-			
 			::if (assets != null)::::foreach assets::::if (type == "font")::className.set ("::id::", nme.NME_::flatName::);
-			::else::path.set ("::id::", "::resourceName::");
-			::end::type.set ("::id::", Reflect.field (AssetType, "::type::".toUpperCase ()));
+			::else::path.set("::id::", "::resourceName::");
+			::end::type.set("::id::", "::type::".toUpperCase());
 			::end::::end::
-			::if (libraries != null)::::foreach libraries::library.set ("::name::", Reflect.field (LibraryType, "::type::".toUpperCase ()));
+			::if (libraries != null)::::foreach libraries::library.set ("::name::", "::type::");
 			::end::::end::
 			initialized = true;
-			
 		}
-		
 	}
-	
-	
 }
-
 
 ::foreach assets::::if (type == "font")::class NME_::flatName:: extends flash.text.Font { }
 ::end::::end::
