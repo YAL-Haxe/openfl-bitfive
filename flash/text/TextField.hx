059a0e2460035e5e1d3476e1726dcd690867b6e8
YellowAfterlife <yellowafterlife@ymail.com>
2014-03-03 09:05:00 +0200

    Fixed missing argument in TextField.hitTestLocal
    Had a feeling that I forgot to introduce new argument somewhere. Wasn't
    wrong...

--- flash/text/TextField.hx
+++ flash/text/TextField.hx
@@ -280,7 +280,7 @@ class TextField extends flash.display.InteractiveObject implements IBitmapDrawab
 		return qEditable ? qTextArea.value.substring(a, b) : null;
 	}
 	// event target overrides:
-	override public function hitTestLocal(x:Float, y:Float):Bool {
+	override public function hitTestLocal(x:Float, y:Float, p:Bool):Bool {
 		return x >= 0 && y >= 0 && x < width && y < height;
 	}
 	override public function addEventListener(type:String, listener:Dynamic -> Void, useCapture:Bool = false, priority:Int = 0, weak:Bool = false):Void {
