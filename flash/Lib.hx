c03339e349010ec29d8072fe850279e9ee345355
YellowAfterlife <yellowafterlife@ymail.com>
2014-03-06 15:47:09 +0200

    Added Lib.window, Lib.document, Lib.console
    Hopefully, no collisions to be found.

--- flash/Lib.hx
+++ flash/Lib.hx
@@ -4,13 +4,20 @@ import flash.display.MovieClip;
 import flash.display.Sprite;
 import flash.display.Stage;
 import js.html.CanvasElement;
+import js.html.Console;
 import js.html.CSSStyleDeclaration;
 import js.html.DivElement;
+import js.html.Document;
+import js.html.DOMWindow;
 import js.html.Element;
 
 class Lib {
 	public static var current(get, null):MovieClip;
 	public static var stage(get, null):Stage;
+	//
+	public static var window(get, null):DOMWindow;
+	public static var document(get, null):Document;
+	public static var console(get, null):Console;
 	//
 	private static var qCurrent:MovieClip;
 	private static var qStage:Stage;
@@ -30,17 +37,18 @@ class Lib {
 		schList = [];
 		schLength = 0;
 		// bind a "delay" method:
-		untyped window.reqAnimFrame =
-				window.requestAnimationFrame ||
-				window.webkitRequestAnimationFrame ||
-				window.mozRequestAnimationFrame ||
-				window.oRequestAnimationFrame ||
-				window.msRequestAnimationFrame ||
+		var wnd = window;
+		untyped wnd.reqAnimFrame =
+				wnd.requestAnimationFrame ||
+				wnd.webkitRequestAnimationFrame ||
+				wnd.mozRequestAnimationFrame ||
+				wnd.oRequestAnimationFrame ||
+				wnd.msRequestAnimationFrame ||
 				function (o) {
-					untyped window.setTimeout(o, 700 / Lib.frameRate, null);
+					wnd.setTimeout(o, Std.int(700 / Lib.frameRate));
 				};
 		// possibly bulky mobile browser detection (from detectmobilebrowsers.com):
-		o = untyped navigator.userAgent || navigator.vendor || window.opera;
+		o = untyped navigator.userAgent || navigator.vendor || wnd.opera;
 		mobile = untyped __js__("/(android|bb\\d+|meego).+mobile|avantgo|bada\\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od|ad)|iris|kindle|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i").test(o)
 		|| __js__("/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\\-(n|u)|c55\\/|capi|ccwa|cdm\\-|cell|chtm|cldc|cmd\\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\\-s|devi|dica|dmob|do(c|p)o|ds(12|\\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\\-|_)|g1 u|g560|gene|gf\\-5|g\\-mo|go(\\.w|od)|gr(ad|un)|haie|hcit|hd\\-(m|p|t)|hei\\-|hi(pt|ta)|hp( i|ip)|hs\\-c|ht(c(\\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\\-(20|go|ma)|i230|iac( |\\-|\\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\\/)|klon|kpt |kwc\\-|kyo(c|k)|le(no|xi)|lg( g|\\/(k|l|u)|50|54|\\-[a-w])|libw|lynx|m1\\-w|m3ga|m50\\/|ma(te|ui|xo)|mc(01|21|ca)|m\\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\\-2|po(ck|rt|se)|prox|psio|pt\\-g|qa\\-a|qc(07|12|21|32|60|\\-[2-7]|i\\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\\-|oo|p\\-)|sdk\\/|se(c(\\-|0|1)|47|mc|nd|ri)|sgh\\-|shar|sie(\\-|m)|sk\\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\\-|v\\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\\-|tdg\\-|tel(i|m)|tim\\-|t\\-mo|to(pl|sh)|ts(70|m\\-|m3|m5)|tx\\-9|up(\\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\\-|your|zeto|zte\\-/i").test(o.substr(0,4));
 	}
@@ -51,14 +59,18 @@ class Lib {
 	}
 	public static function getTimer():Int {
 		inline function dateNow():Float return cast Date.now();
-		return untyped Std.int(dateNow() - qTimeStamp);
+		return Std.int(dateNow() - qTimeStamp);
 	}
 	public static function getURL(url:flash.net.URLRequest, ?target:String) {
-		untyped window.open(url.url, target);
+		window.open(url.url, target);
 	}
 	//
+	@:extern private static inline function get_window() return untyped __js__("window");
+	@:extern private static inline function get_document() return untyped __js__("document");
+	@:extern private static inline function get_console() return untyped __js__("console");
+	//
 	public static function jsNode(o:String):Element {
-		var r:Element = untyped document.createElement(o), s:js.html.CSSStyleDeclaration = r.style;
+		var r:Element = document.createElement(o), s:CSSStyleDeclaration = r.style;
 		s.position = 'absolute';
 		switch (o) {
 		case "canvas":
@@ -84,7 +96,7 @@ class Lib {
 			get_stage().component.appendChild(o);
 			o.style.visibility = "hidden";
 			#if debug
-				o.setAttribute("node", "Lib.jsHelper");
+				o.setAttribute("node", "flash.Lib.jsHelper");
 			#end
 			o.appendChild(qHelper = jsDiv());
 		}
@@ -98,7 +110,7 @@ class Lib {
 	}
 	private static function get_stage():Stage {
 		if (qStage == null) {
-			untyped document.body.appendChild((qStage = new Stage()).component);
+			document.body.appendChild((qStage = new Stage()).component);
 		}
 		return qStage;
 	}
