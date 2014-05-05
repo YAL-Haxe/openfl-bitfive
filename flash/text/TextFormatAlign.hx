21c504f3ee1bba85f0320607016d3aba521307e0
YellowAfterlife <yellowafterlife@ymail.com>
2014-05-05 19:12:39 +0300

    Switched TextFormatAlign to abstract.

--- flash/text/TextFormatAlign.hx
+++ flash/text/TextFormatAlign.hx
@@ -1,9 +1,15 @@
 package flash.text;
 #if js
-@:fakeEnum(String) enum TextFormatAlign {
-	LEFT;
-	RIGHT;
-	JUSTIFY;
-	CENTER;
+abstract TextFormatAlign(String) {
+	@:extern public static inline var LEFT:TextFormatAlign = "LEFT";
+	@:extern public static inline var RIGHT:TextFormatAlign = "RIGHT";
+	@:extern public static inline var CENTER:TextFormatAlign = "CENTER";
+	@:extern public static inline var JUSTIFY:TextFormatAlign = "JUSTIFY";
+	//
+	@:extern public inline function new(o:String) this = o;
+	@:extern @:to public inline function toString():String return this;
+	@:extern @:from public static inline function fromString(s:String) {
+		return new TextFormatAlign(s);
+	}
 }
 #end
