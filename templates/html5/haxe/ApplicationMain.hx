eee2ad961ea1b718725ee9003adb87ecb912093f
YellowAfterlife <yellowafterlife@ymail.com>
2014-11-28 03:09:31 +0200

    Added ApplicationMain.config

--- templates/html5/haxe/ApplicationMain.hx
+++ templates/html5/haxe/ApplicationMain.hx
@@ -14,6 +14,23 @@ import js.html.Element;
 import js.html.AudioElement;
 
 class ApplicationMain {
+	#if (openfl >= "2.1")
+	public static var config:lime.app.Config = {
+		antialiasing: Std.int(::WIN_ANTIALIASING::),
+		background: Std.int(::WIN_BACKGROUND::),
+		borderless: ::WIN_BORDERLESS::,
+		depthBuffer: ::WIN_DEPTH_BUFFER::,
+		fps: Std.int(::WIN_FPS::),
+		fullscreen: ::WIN_FULLSCREEN::,
+		height: Std.int(::WIN_HEIGHT::),
+		orientation: "::WIN_ORIENTATION::",
+		resizable: ::WIN_RESIZABLE::,
+		stencilBuffer: ::WIN_STENCIL_BUFFER::,
+		title: "::APP_TITLE::",
+		vsync: ::WIN_VSYNC::,
+		width: Std.int(::WIN_WIDTH::),
+	};
+	#end
 	private static var completed:Int;
 	private static var preloader:::if (PRELOADER_NAME != "")::::PRELOADER_NAME::::else::NMEPreloader::end::;
 	private static var total:Int;
